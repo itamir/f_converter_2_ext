11183f2a53326d0407236cdba397fccee53f7d9f
Itamir <itamir@Mac-mini-de-itamir.local>
2022-08-26 14:41:16 -0300

    gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 
 const request =
-    "https://api.hgbrasil.com/finance?format=json-cors&key=60c2ffae";
+    "https://api.hgbrasil.com/finance?format=json-cors&key=2a34b926";
 
 void main() async {
   runApp(MaterialApp(
